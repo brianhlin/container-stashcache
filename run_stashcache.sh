b4c7626fc167d002b0f917ab03f6500065389e32
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-06-21 16:35:14 -0500

    testing again

--- run_stashcache.sh
+++ run_stashcache.sh
@@ -3,4 +3,7 @@
 env
 echo "Starting cache ..."
 su -p xrootd -s /bin/sh -c "echo 'hello world'"
+ls -l /var/log/xrootd/xrootd.log
+ls -l /etc/xrootd/xrootd-stashcache-cache-server.cfg
+
 su -p xrootd -s /bin/sh -c "/usr/bin/xrootd -l /var/log/xrootd/xrootd.log -c /etc/xrootd/xrootd-stashcache-cache-server.cfg -k fifo -s /var/run/xrootd/xrootd-stashcache-cache-server.pid -n stashcache-cache-server"
