f2687186ea50ca36f89b23463fbbd03fb9ddfce8
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-06-21 15:14:55 -0500

    trying something

--- run_stashcache.sh
+++ run_stashcache.sh
@@ -2,7 +2,5 @@
 
 env
 echo "Starting cache ..."
-[ -z "$STASHCACHE_SPACE_LO_MARK" ] && STASHCACHE_SPACE_LO_MARK="0.75"
-[ -z "$STASHCACHE_SPACE_HI_MARK" ] && STASHCACHE_SPACE_HI_MARK="0.85"
-
+sleep infinity
 su -p xrootd -s /bin/sh -c "/usr/bin/xrootd -l /var/log/xrootd/xrootd.log -c /etc/xrootd/xrootd-stashcache-cache-server.cfg -k fifo -s /var/run/xrootd/xrootd-stashcache-cache-server.pid -n stashcache-cache-server"
