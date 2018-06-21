2edc4e5e36ff69774da9e8ba2cfa1d57f8485bdf
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-06-21 15:41:54 -0500

    ffs

--- run_stashcache.sh
+++ run_stashcache.sh
@@ -2,5 +2,5 @@
 
 env
 echo "Starting cache ..."
-sleep infinity
+
 su -p xrootd -s /bin/sh -c "/usr/bin/xrootd -l /var/log/xrootd/xrootd.log -c /etc/xrootd/xrootd-stashcache-cache-server.cfg -k fifo -s /var/run/xrootd/xrootd-stashcache-cache-server.pid -n stashcache-cache-server"
