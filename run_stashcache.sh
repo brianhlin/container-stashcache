605507db1e98d6c782571772b68e899a1c83ffca
cnweaver <cnweaver@gmail.com>
2018-10-30 15:54:14 -0500

    Turn debug output back off

--- run_stashcache.sh
+++ run_stashcache.sh
@@ -2,4 +2,4 @@
 
 env
 echo "Starting cache ..."
-su -p xrootd -s /bin/sh -c "/usr/bin/xrootd -d -c /etc/xrootd/xrootd-stashcache-cache-server.cfg -k fifo -s /var/run/xrootd/xrootd-stashcache-cache-server.pid -n stashcache-cache-server"
+su -p xrootd -s /bin/sh -c "/usr/bin/xrootd -c /etc/xrootd/xrootd-stashcache-cache-server.cfg -k fifo -s /var/run/xrootd/xrootd-stashcache-cache-server.pid -n stashcache-cache-server"
